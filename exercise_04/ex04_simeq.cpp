ffb013b92459739da44f0e51ecb45dee69c12698
Stefano Lonardoni <steve.lonny@outlook.com>
2024-06-17 00:20:52 +0200

    ex04 final steps

--- exercise_04/ex04_simeq.cpp
+++ exercise_04/ex04_simeq.cpp
@@ -17,19 +17,21 @@ int main (int argc, char *argv[]){
     properties.writeout = true;    
     //Setting the base path for the input and output files
     array<string, 3> names = {"SOLID", "LIQUID", "GAS"};
-    array<int, 3> b_count = {0, 0, 0}; //thread monitoring
+    //thread monitoring
+    array<int, 3> b_count = {0, 0, 0}; 
     //Starting values for the three phases
     array<int, 3> n_blck = {50, 50, 100}; 
     array<int, 3> n_steps = {2000, 2000, 2000};
+    array<int, 3> n_steps_eq = {50000, 50000, 500000};
     array<double, 3> rho = {1.1, 0.8, 0.05};
     array<double, 3> r_cut = {2.2, 2.5, 5.0};
-    array<double, 3> temp = {0.8, 1.1, 1.2}; //starting temps should not matter when velocities.in is present or when using verlet (?)
+    array<double, 3> temp = {1.54, 1.98, 0.95}; //starting temps should not matter when velocities.in is present or when using verlet (?)
     
     #pragma omp parallel for
     for(int l=0; l<3; l++){ //loop over phases
         //Setting input parameters
         system_input input;
-        input.restart = 1;
+        input.restart = 0;
         input.rho = rho[l];
         input.r_cut = r_cut[l];
         input.temp = temp[l];
@@ -37,15 +39,23 @@ int main (int argc, char *argv[]){
         input.nsteps = n_steps[l];
         int nconf{1};
         //input - ouput files
-        string filein = "ex04/INPUT_" + names[l];
-        string fileout = "ex04/OUTPUT_" + names[l];
+        //string filein = "ex04/INPUT_" + names[l];
+        string fileout = "ex04/OUTPUT_EQ_" + names[l];
+        /*
         if(!std::filesystem::exists(paths::path_DATA / filein / "CONFIG/velocities.in")){
             std::filesystem::rename (paths::path_DATA / filein / "CONFIG/velocities.out" , paths::path_DATA / filein / "CONFIG/velocities.in");
         }
+        */
         //initialize system
-        System SYS(paths::path_DATA / filein, paths::path_DATA / fileout);
+        System SYS(paths::path_ROOT / "exercise_04/INPUT", paths::path_DATA / fileout);
         SYS.initialize(input);
         SYS.initialize_properties(properties);
+        SYS.block_reset(0);
+        fmt::print("Starting equilibration for {0} phase\n", names[l]);
+        for(int i{0}; i<n_steps_eq[l]; i++){ //equilibration steps
+            SYS.step();
+            //SYS.measure();
+        }
         SYS.block_reset(0);
         for(int j{0}; j < SYS.get_nbl(); j++){ //loop over blocks
             for(int k{0}; k < SYS.get_nsteps(); k++){ //loop over steps in a block
