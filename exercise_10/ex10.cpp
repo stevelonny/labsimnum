8890e751b21529f5636a177c5a108f189baeb4de
stevelonny <steve.lonny@outlook.com>
2024-06-22 10:03:58 +0200

    polished err codes and seed setting for arma

--- exercise_10/ex10.cpp
+++ exercise_10/ex10.cpp
@@ -15,7 +15,7 @@ int main(int argc, char* argv[]){
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    if(size < 3 || size > 21){
+    if(size < 3 || size > 11){
         fmt::print("This program must be run with 3 to 11 processes\n");
         MPI_Finalize();
         return 1;
@@ -26,7 +26,6 @@ int main(int argc, char* argv[]){
     if(rank == 0){
         for(int i{0}; i<size-1; i++){
             currentgens[i] = 0;
-            // indexes[i] = atlas->getNCities()+1;
         }        
     }
 
@@ -58,9 +57,15 @@ int main(int argc, char* argv[]){
         MPI_Bcast(coords.memptr(), coords.size(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
         atlas->InitCoords(coords);
     }
-    if((argc < 2 || argc >4)  || ((strcmp(argv[1], "circle") != 0 || strcmp(argv[1], "square") != 0) || strcmp(argv[1], "provita") != 0)){
-        fmt::print(cerr, "Usage: <mapper> [<seltype>]\nwhere <mapper> is either 'circle' or 'square' or 'provita'\n", argv[0]);
-        fmt::print(cerr, "and optionally <seltype> is either 0(lin rank-based) 1(roulette) 2(tournament) 3(stoch-univ-acc)\n");
+    else if((argc < 2 || argc > 4)){
+        if(rank == 0) fmt::print(cerr, "Usage: {0} <mapper> [<seltype>]\nwhere <mapper> is either 'circle' or 'square' or 'provita'\n", argv[0]);
+        if(rank == 0) fmt::print(cerr, "and optionally <seltype> is either 0(lin rank-based) 1(roulette) 2(tournament) 3(stoch-univ-acc)\n");
+        MPI_Finalize();
+        return 1;
+        }
+    else{
+        if(rank == 0) fmt::print(cerr, "Usage: {0} <mapper> [<seltype>]\nwhere <mapper> is either 'circle' or 'square' or 'provita'\n", argv[0]);
+        if(rank == 0) fmt::print(cerr, "and optionally <seltype> is either 0(lin rank-based) 1(roulette) 2(tournament) 3(stoch-univ-acc)\n");
         MPI_Finalize();
         return 1;
     }
@@ -86,54 +91,7 @@ int main(int argc, char* argv[]){
         fileout.clear();
     }
 
-/*
-    if(rank==0){
-        int gen{0};
-        string filename = fmt::format("{0}/ex10_atlas.dat", paths::path_DATA.string(), argv[1]);
-        ofstream fileout(filename.c_str());
-        for(int i{0}; i<atlas->getNCities(); i++){
-            fmt::print(fileout, "{0:<5} {1:<10.5f} {2:<10.5f}\n", i, atlas->Position(i)[0], atlas->Position(i)[1]);
-        }
-        fileout.close();
-        fileout.clear();
-        Random rnd(0);
-        arma::imat tributes(atlas->getNCities(), size-1);
-        while(gen < 490){
-            // tributes.set_size(atlas->getNCities(), size-1);
-            gen += 10;
-            fmt::print(cerr, "Waiting for champions from others continents...\n");
-            for(int i{1}; i<size; i++){
-                MPI_Recv(tributes.colptr(i-1), tributes.n_rows, MPI_INTEGER8, i, i, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                fmt::print(cerr, "Received champion from continent {0}\n", i );
-            }
-            int dest{1};
-            while(0<tributes.n_cols){
-                int champion = static_cast<int>(rnd.Rannyu(0, tributes.n_cols));
-                MPI_Send(tributes.colptr(champion), tributes.n_rows, MPI_INTEGER8, dest, 0, MPI_COMM_WORLD);
-                // MPI_Send(homecoming.memptr(), homecoming.size(), MPI_INT, i, 0, MPI_COMM_WORLD);
-                dest++;
-                tributes.shed_col(champion);
-            }
-            // MPI_Bcast(indexes, size-1, MPI_INT, 0, MPI_COMM_WORLD);
-            // MPI_Gather(&r_gen, 1, MPI_INT, currentgens, 1, MPI_INT, 0, MPI_COMM_WORLD);
-            fmt::print("Generations: {0}\n", gen);
-            tributes.set_size(atlas->getNCities(), size-1);
-        }
-        fmt::print("Done\n");
-        for(int i{1}; i<size; i++){
-            arma::ivec best(atlas->getNCities());
-            MPI_Recv(tributes.colptr(i-1), tributes.n_rows, MPI_INTEGER8, i, 10+i, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            fmt::print(cerr, "Received best from continent {0}\n", i);
-        }
-        tributes.print("Bests");
-        tributes.save(fmt::format("{0}/ex10_bests.dat", paths::path_DATA), arma::raw_ascii);
-    }
-*/
-
-    // initilize rnd with different seed for each rank
-    // Random rnd(rank);
     Population pop(atlas, 200);
-    // fmt::print(cerr, "{0:<3}: atlas ptr counter {1}\n", rank, atlas.use_count());
     BattleRoyale pubg(rank, seltype);
     /* if(rank == 0)  */tributes.set_size(atlas->getNCities(), size);
     filename = fmt::format("{0}/ex10_r{2}_values_{1}_{3}.dat", paths::path_DATA.string(), argv[1], rank, seltype);
@@ -148,36 +106,11 @@ int main(int argc, char* argv[]){
             arma::ivec tribute = pop._apopulation.col(champion);
             //arma::ivec homecoming(atlas->getNCities());
             MPI_Gather(tribute.memptr(), tribute.size(), MPI_INTEGER8, tributes.memptr(), tribute.size(), MPI_INTEGER8, 0, MPI_COMM_WORLD);
-            if(rank == 0) tributes = shuffle(tributes, 1);
+            if(rank == 0) {
+                arma_rng::set_seed(0);
+                tributes = shuffle(tributes, 1);
+                }
             MPI_Bcast(tributes.memptr(), tributes.size(), MPI_INTEGER8, 0, MPI_COMM_WORLD);
-            /*
-            if(rank == 0){
-                tributes.col(0) = tribute;
-                for(int i{1}; i<size; i++){
-                    MPI_Recv(tributes.colptr(i), tributes.n_rows, MPI_INTEGER8, i, i, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                    fmt::print("Received champion from continent {0}\n", i);
-                }
-                int champ_0 = static_cast<int>(rnd.Rannyu(0, tributes.n_cols));
-                pop._apopulation.col(champion) = tributes.col(champ_0);
-                tributes.shed_col(champ_0);
-                int dest{1};
-                while(0<tributes.n_cols){
-                    int chosen = static_cast<int>(rnd.Rannyu(0, tributes.n_cols));
-                    MPI_Send(tributes.colptr(chosen), tributes.n_rows, MPI_INTEGER8, dest, 0, MPI_COMM_WORLD);
-                    dest++;
-                    tributes.shed_col(chosen);
-                }
-                tributes.set_size(atlas->getNCities(), size);
-            }
-            */
-            /*
-            else {
-                MPI_Send(tribute.memptr(), tribute.size(), MPI_INTEGER8, 0, rank, MPI_COMM_WORLD);
-                MPI_Recv(homecoming.memptr(), homecoming.size(), MPI_INTEGER8, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                fmt::print(cerr, "{1:<3}:Received champion from continent {0}\n", 0, rank);
-                pop._apopulation.col(champion) = homecoming;
-            }
-            */
             pop._apopulation.col(champion) = tributes.col(rank);
             MPI_Gather(&r_gen, 1, MPI_INT, currentgens, 1, MPI_INT, 0, MPI_COMM_WORLD);
             if(rank == 0) fmt::print("Generations: {0}\r", currentgens[0]);
@@ -189,17 +122,9 @@ int main(int argc, char* argv[]){
     // send best to root process
     pop.GiveDistance();
     arma::ivec best = pop._apopulation.col(pop.getBestIndex());
-    // if(rank != 0) MPI_Send(best.memptr(), best.size(), MPI_INTEGER8, 0, 10+rank, MPI_COMM_WORLD);
     // root process receives and saves bests
     MPI_Gather(best.memptr(), best.size(), MPI_INTEGER8, tributes.memptr(), best.size(), MPI_INTEGER8, 0, MPI_COMM_WORLD);
     if(rank == 0){
-        // tributes.col(0) = best;
-        /*
-        for(int i{1}; i<size; i++){
-            MPI_Recv(tributes.colptr(i), tributes.n_rows, MPI_INTEGER8, i, 10+i, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            fmt::print("Received best from continent {0}\n", i);
-        }
-        */
         fmt::print("\n");
         tributes.brief_print("Bests");
         fmt::print("Done. Writing champions to file ");
@@ -219,10 +144,7 @@ int main(int argc, char* argv[]){
             fmt::print(fileout, "{0:<5} {1:<10.5f} {2:<10.5f}\n", best(i), atlas.get()->Position(best(i))[0], atlas.get()->Position(best(i))[1]);
         }
         fileout.close();
-        // fmt::print(cerr, "{0:<3}: atlas ptr counter {1}\n", rank, atlas.use_count());
     }
-    // fmt::print(cerr, "{0:<3}: atlas ptr counter {1}\n", rank, atlas.use_count());
-    // atlas.unique();
     delete[] currentgens;
     MPI_Finalize();
     return  0;
